c84fbb777bf2c81d78438a0f384930fcafe6b5a3
Raahim3108 <raahim94@gmail.com>
2022-08-27 21:02:36 +0200

    initial commit

---
+++ prac_artificial_neural_network.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 15 21:21:34 2020
+
+@author: Raahim
+"""
+
+import numpy as np 
+import pandas as pd 
+import tensorflow as tf 
+
+print(tf.__version__)
+
+dataset = pd.read_excel('Folds5x2_pp.xlsx')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+ann = tf.keras.models.Sequential()
+
+ann.add(tf.keras.layers.Dense(units = 6, activation = 'relu'))
+ann.add(tf.keras.layers.Dense(units = 6, activation = 'relu'))
+
+ann.add(tf.keras.layers.Dense(units = 1))
+
+ann.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+ann.fit(X_train, y_train, batch_size = 32, epochs = 100)
+
+y_pred = ann.predict(X_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
